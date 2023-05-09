7f03105c35f94687be2e0922df4e1f41cf3068a0
dandanmatunoshita <dandanmatunoshita@gmail.com>
2023-05-09 13:10:46 -0300

    update-card

--- src/app/dashboard/cards/cards.component.ts
+++ src/app/dashboard/cards/cards.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
 
+export class CardsComponent implements OnInit{
+  isChecked: boolean = false;
+
+  ngOnInit() {
+    const card: HTMLElement = document.getElementById("card2")!;
+    const card2: HTMLElement = document.getElementById("checkbox2")!;
+    const checkbox: HTMLInputElement = document.getElementById("checkbox2") as HTMLInputElement;
+
+
+    card.addEventListener("click", () => {
+      this.isChecked = !this.isChecked;
+      checkbox.checked = this.isChecked;
+    });
+    card2.addEventListener("click", () => {
+      this.isChecked = !this.isChecked;
+      checkbox.checked = this.isChecked;
+    });
+  }
 
 }
--- src/app/management/card-management/card-management.component.ts
+++ src/app/management/card-management/card-management.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-management',
   templateUrl: './card-management.component.html',
   styleUrls: ['./card-management.component.scss']
 })
-export class CardManagementComponent {
+
+export class CardManagementComponent implements OnInit {
+
+  isChecked: boolean = false;
+
+  ngOnInit() {
+    const card: HTMLElement = document.getElementById("card")!;
+    const card2: HTMLElement = document.getElementById("checkbox")!;
+    const checkbox: HTMLInputElement = document.getElementById("checkbox") as HTMLInputElement;
+
+
+    card.addEventListener("click", () => {
+      this.isChecked = !this.isChecked;
+      checkbox.checked = this.isChecked
+    });
+    card2.addEventListener("click", () => {
+      this.isChecked = !this.isChecked;
+      checkbox.checked = this.isChecked
+    });
+  }
 
 }
