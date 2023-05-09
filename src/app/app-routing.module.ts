cda789da8bc0cab4addfcaa2b5fc73489cd3aa98
dandanmatunoshita <dandanmatunoshita@gmail.com>
2023-05-09 10:10:28 -0300

    update-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { ManagementComponent } from "./pages/management/management.component";
 
 
 const APP_ROUTES: Routes = [
     { path: "dashboard", component: DashboardComponent},
-    { path: '', component: DashboardComponent}
+    { path: '', component: DashboardComponent},
+    { path: "management", component: ManagementComponent}
 ];
 
 @NgModule({
