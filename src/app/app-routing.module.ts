bff559236453c519ab68a64589ef1295444ea3d1
Matheus Rezende <72827374+MRezende0@users.noreply.github.com>
2023-05-11 09:36:55 -0300

    update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CookiesComponent } from './cookies/cookies.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { RegisterComponent } from './pages/register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CookiesComponent } from './cookies/cookies.component';
+import { EditUserprofileComponent } from './edit-userprofile/edit-userprofile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ManagementComponent } from "./pages/management/management.component";
+import { PrivacyComponent } from './privacy/privacy.component';
+import { Register2Component } from './pages/register2/register2.component';
+import { Register3Component } from './pages/register3/register3.component';
+
 
 //IMPORTS DOS COMPONENTES DO PORTAL
 
@@ -8,6 +20,17 @@ const routes: Routes = [
 
   // -----ROTAS DO PORTAL --------
   // { path: 'cookies', component: CookiesComponent },
+  // { path: "dashboard", component: DashboardComponent},
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'user-profile', component: UserProfileComponent },
+  // { path: 'cookies', component: CookiesComponent },
+  // { path: 'edit-userprofile', component: EditUserprofileComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: '', component: LoginComponent },
+  // { path: "management", component: ManagementComponent},
+  // { path: 'privacy', component: PrivacyComponent },
+  // { path: 'register2', component: Register2Component },
+  // { path: 'register3', component: Register3Component },
 
 ];
 
