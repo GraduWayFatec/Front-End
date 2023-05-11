f375ab24f376baf23c697fa2f0000d2205692f50
dandanmatunoshita <dandanmatunoshita@gmail.com>
2023-05-11 09:27:34 -0300

    edit router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ModuleWithProviders } from "@angular/core";
 
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { ManagementComponent } from "./pages/management/management.component";
 
 
-const APP_ROUTES: Routes = [
+const routes: Routes = [
     { path: "dashboard", component: DashboardComponent},
     { path: '', component: DashboardComponent},
     { path: "management", component: ManagementComponent}
 ];
 
 @NgModule({
-    declarations: [],
-    imports: [RouterModule.forRoot(APP_ROUTES)],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 
-export class AppRoutingModule{
-    static routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES);
-}
+export class AppRoutingModule{  }
