54d3b67a98f1de4f090f47b71b4d98b53b1a36b2
Matheus Rezende <72827374+MRezende0@users.noreply.github.com>
2023-05-11 09:52:17 -0300

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,16 +9,39 @@ import { CardsComponent } from './dashboard/cards/cards.component';
 
 import { SearchComponent } from './dashboard/search/search.component';
 
+// import { CookiesComponent } from './cookies/cookies.component';
+// import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+// import { RegisterComponent } from './pages/register/register.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { CookiesComponent } from './cookies/cookies.component';
+// import { EditUserprofileComponent } from './edit-userprofile/edit-userprofile.component';
+// import { LoginComponent } from './pages/login/login.component';
+// import { ManagementComponent } from "./pages/management/management.component";
+// import { PrivacyComponent } from './privacy/privacy.component';
+// import { Register2Component } from './pages/register2/register2.component';
+// import { Register3Component } from './pages/register3/register3.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
     CardsComponent,
-    SearchComponent
+    SearchComponent,
+    // CookiesComponent,
+    // DashboardComponent,
+    // RegisterComponent,
+    // Register2Component,
+    // Register3Component,
+    // UserProfileComponent,
+    // EditUserprofileComponent,
+    // LoginComponent,
+    // ManagementComponent,
+    // PrivacyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
