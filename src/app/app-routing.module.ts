78b4601f10974479802bc6b51ba46d0c69f54b4a
Matheus Rezende <matheus_rezende0@hotmail.com>
2023-05-11 13:17:58 -0300

    add-modal-message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { RouterModule, Routes } from '@angular/router';
 // import { PrivacyComponent } from './privacy/privacy.component';
 // import { Register2Component } from './pages/register2/register2.component';
 // import { Register3Component } from './pages/register3/register3.component';
+import { MessageComponent } from './message/message.component';
 
 
 //IMPORTS DOS COMPONENTES DO PORTAL
@@ -19,6 +20,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   // -----ROTAS DO PORTAL --------
+  { path: "message", component: MessageComponent},
   // { path: "dashboard", component: DashboardComponent},
   // { path: 'register', component: RegisterComponent },
   // { path: 'user-profile', component: UserProfileComponent },
