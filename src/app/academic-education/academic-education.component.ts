456020b90fa57b01f5c02bf42c5a6b28080380c1
JacqueNakagawa <jacquelinenakagawa@gmail.com>
2023-05-12 09:10:42 -0300

    add-modal-academic-education

---
+++ src/app/academic-education/academic-education.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academic-education',
+  templateUrl: './academic-education.component.html',
+  styleUrls: ['./academic-education.component.scss']
+})
+export class AcademicEducationComponent {
+
+}
