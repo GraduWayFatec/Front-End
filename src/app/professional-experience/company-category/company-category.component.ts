a55dda174c12342bca5508a9ce71d8d2f5c44604
JacqueNakagawa <jacquelinenakagawa@gmail.com>
2023-05-13 11:55:50 -0300

    update-dropdown-company-category

--- src/app/professional-experience/company-category/company-category.component.ts
+++ src/app/professional-experience/company-category/company-category.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/shared/company-category.model'
 
 @Component({
   selector: 'app-company-category',
   templateUrl: './company-category.component.html',
   styleUrls: ['./company-category.component.scss']
 })
-export class CompanyCategoryComponent {
+export class CompanyCategoryComponent implements OnInit {
 
+  checkboxes: Category[] = [
+    new Category('Pública'),
+    new Category('Privada'),
+    new Category('Mista'),
+    new Category('Terceiro Setor'),
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+
+  }
 }
