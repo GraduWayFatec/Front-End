0158a685aa473711e9a7dd2770d75b79be1ac8e8
dandanmatunoshita <dandanmatunoshita@gmail.com>
2023-05-16 10:01:56 -0300

    add-modalModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -38,6 +39,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ModalModule.forRoot(),
     MatDialogModule,
     BrowserAnimationsModule
   ],
